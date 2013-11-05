6ac8e1a4de3cd95619501b65f8d6d235803a12ea
Richard Davis <rdavis.bacs@gmail.com>
2013-11-05 17:54:09 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UpNext::Application.config.secret_key_base = '102befb42c09cd4c2a72e1c2e5597818e05c21c5ecb88481c8f611b3ed64ec5d748ba14117a1e2d81405bb284ab1e87ad14576b63f95e33e65b0eac9554dad0f'
