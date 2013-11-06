bcc4dc10534b7685cd924dd6f6443eaea8cdc661
Richard Davis <rdavis.bacs@gmail.com>
2013-11-06 17:52:41 -0500

    validations

--- app/models/eztv_items.rb
+++ app/models/eztv_items.rb
@@ -1,2 +1,6 @@
 class EztvItems < ActiveRecord::Base
+  validates_presence_of :show_name
+  validates_inclusion_of :season, in: (1..99)
+  validates_inclusion_of :episode, in: (1..9999)
+  validates_format_of :link, with: /http:\/\/.*.torrent/
 end
--- db/migrate/20131105225819_create_eztv_items.rb
+++ db/migrate/20131105225819_create_eztv_items.rb
@@ -1,11 +1,11 @@
 class CreateEztvItems < ActiveRecord::Migration
   def change
     create_table :eztv_items do |t|
-      t.string :show_name
+      t.string :show_name, null: false
       t.string :episode_title
       t.integer :season
       t.integer :episode
-      t.string :link
+      t.string :link, null: false
       t.string :description
       t.string :comments
       t.string :category
